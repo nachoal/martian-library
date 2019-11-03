b8c16b0930155712eda0f21be18f776c430e8024
Ignacio Alonso <ignacio.alley@gmail.com>
2019-11-03 11:42:47 -0600

    Create models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+nacho = User.create!(
+  email: 'ignacio@codia.co',
+  first_name: 'Ignacio',
+  last_name: 'Alonso'
+)
+
+andrea = User.create!(
+  email: 'andrea@codia.co',
+  first_name: 'Andrea',
+  last_name: 'Bazan'
+)
+
+Item.create!(
+  [
+    {
+      title: "Martian Chronicles",
+      description: "Cult book by Ray Bradbury",
+      user: nacho,
+      image_url: "https://upload.wikimedia.org/wikipedia/en/4/45/The-Martian-Chronicles.jpg"
+    },
+    {
+      title: "The Martian",
+      description: "Novel by Andy Weir about an astronaut stranded on Mars trying to survive",
+      user: nacho,
+      image_url: "https://upload.wikimedia.org/wikipedia/en/c/c3/The_Martian_2014.jpg"
+    },
+    {
+      title: "Doom",
+      description: "A group of Marines is sent to the red planet via an ancient " \
+                   "Martian portal called the Ark to deal with an outbreak of a mutagenic virus",
+      user: andrea,
+      image_url: "https://upload.wikimedia.org/wikipedia/en/5/57/Doom_cover_art.jpg"
+    },
+    {
+      title: "Mars Attacks!",
+      description: "Earth is invaded by Martians with unbeatable weapons and a cruel sense of humor",
+      user: andrea,
+      image_url: "https://upload.wikimedia.org/wikipedia/en/b/bd/Mars_attacks_ver1.jpg"
+    }
+  ]
+)
